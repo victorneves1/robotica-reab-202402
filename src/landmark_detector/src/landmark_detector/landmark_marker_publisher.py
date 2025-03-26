6ccc8f407f9535c0c5e47a85d1011a3b3fcdc594
Joao Felipe Gobeti <joao.gobeti@motora.ai>
2025-03-26 16:09:50 -0300

    namespace being published as object class name

--- src/landmark_detector/src/landmark_detector/landmark_marker_publisher.py
+++ src/landmark_detector/src/landmark_detector/landmark_marker_publisher.py
@@ -9,6 +9,28 @@ from landmark_detector_interfaces.msg import LandmarkArray
 from builtin_interfaces.msg import Duration
 
 
+label_map = {
+    1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorbike', 5: 'aeroplane',
+    6: 'bus', 7: 'train', 8: 'truck', 9: 'boat', 10: 'trafficlight',
+    11: 'firehydrant', 12: 'streetsign', 13: 'stopsign', 14: 'parkingmeter',
+    15: 'bench', 16: 'bird', 17: 'cat', 18: 'dog', 19: 'horse',
+    20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear', 24: 'zebra',
+    25: 'giraffe', 26: 'hat', 27: 'backpack', 28: 'umbrella', 29: 'shoe',
+    30: 'eyeglasses', 31: 'handbag', 32: 'tie', 33: 'suitcase', 34: 'frisbee',
+    35: 'skis', 36: 'snowboard', 37: 'sportsball', 38: 'kite', 39: 'baseballbat',
+    40: 'baseballglove', 41: 'skateboard', 42: 'surfboard', 43: 'tennisracket',
+    44: 'bottle', 45: 'plate', 46: 'wineglass', 47: 'cup', 48: 'fork',
+    49: 'knife', 50: 'spoon', 51: 'bowl', 52: 'banana', 53: 'apple',
+    54: 'sandwich', 55: 'orange', 56: 'broccoli', 57: 'carrot', 58: 'hotdog',
+    59: 'pizza', 60: 'donut', 61: 'cake', 62: 'chair', 63: 'sofa',
+    64: 'pottedplant', 65: 'bed', 66: 'mirror', 67: 'diningtable', 68: 'window',
+    69: 'desk', 70: 'toilet', 71: 'door', 72: 'tv', 73: 'laptop',
+    74: 'mouse', 75: 'remote', 76: 'keyboard', 77: 'cellphone', 78: 'microwave',
+    79: 'oven', 80: 'toaster', 81: 'sink', 82: 'refrigerator', 83: 'blender',
+    84: 'book', 85: 'clock', 86: 'vase', 87: 'scissors', 88: 'teddybear',
+    89: 'hairdrier', 90: 'toothbrush', 91: 'hairbrush'
+}
+
 class LandmarkPublisher(Node):
     def __init__(self):
         super().__init__('landmark_publisher')
@@ -43,7 +65,7 @@ class LandmarkPublisher(Node):
             marker = Marker()
             marker.header.frame_id = 'map'  # or use 'odom' if you'd like the marker in that frame
             marker.header.stamp = self.get_clock().now().to_msg()
-            marker.ns = 'landmark'
+            marker.ns = label_map[int(landmark.label)]
             marker.id = i
             marker.type = Marker.CUBE
             marker.action = Marker.ADD
@@ -58,15 +80,21 @@ class LandmarkPublisher(Node):
 
             # Here we assume the landmark message has x, y, z coordinates
             x, y, z = landmark.x_min, landmark.y_min, landmark.z_min  # Use appropriate coordinates
+            x = x + 0.5 * (landmark.x_max - landmark.x_min)
+            y = y + 0.5 * (landmark.y_max - landmark.y_min)
+            z = z + 0.5 * (landmark.z_max - landmark.z_min)
             
             # Create a PoseStamped for transforming landmark position to the robot frame
             landmark_pose = PoseStamped()
             landmark_pose.header.frame_id = "map"  # This could also be 'odom' depending on your reference frame
             landmark_pose.header.stamp = self.get_clock().now().to_msg()
-            landmark_pose.pose.position.x = x
-            landmark_pose.pose.position.y = y
+            landmark_pose.pose.position.x = x 
+            landmark_pose.pose.position.y = y 
             landmark_pose.pose.position.z = 0.0  # No z coordinate for this marker
             landmark_pose.pose.orientation.w = 1.0  # No rotation for this marker
+            # marker.pose = landmark_pose.pose
+            # marker.orientation.w = 1.0
+            # landmark_array.markers.append(marker)
 
             try:
                 # Transform the landmark pose to the robot's frame (e.g., 'odom')
